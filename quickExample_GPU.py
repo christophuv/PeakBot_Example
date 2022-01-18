b612399177e99abc868f3f1f4dfa6bb0d51c50e9
christophuv <christoph.bueschl@univie.ac.at>
2022-01-18 17:03:36 +0100

    cpu quick example
    added: quick example for cpu functions

--- quickExample_GPU.py
+++ quickExample_GPU.py
@@ -135,7 +135,7 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
         tf.config.experimental.set_virtual_device_configuration(gpus[0],
-       [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+       [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)])
         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
     except RuntimeError as e:
